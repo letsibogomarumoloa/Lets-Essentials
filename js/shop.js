fc8ba692adb79a480f03b402dce86fe8d8103c91
letsibogomarumoloa <letsibogomarumoloa@gmail.com>
2026-07-28 00:45:01 +0200

    Update shop.js

--- js/shop.js
+++ js/shop.js
@@ -1,185 +1,46 @@
-let cart = [];
+// ==================================
+// LETS ESSENTIALS SHOPPING CART
+// ==================================
 
 
 
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 
-function addProduct(name,price){
 
 
-cart.push({
+// Add product to cart
 
-name:name,
+function addToCart(name, price){
 
-price:price
 
-});
+    cart.push({
 
+        name:name,
 
-displayCart();
+        price:price,
 
+        quantity:1
 
-}
+    });
 
 
 
+    localStorage.setItem(
 
+        "cart",
 
-function displayCart(){
+        JSON.stringify(cart)
 
+    );
 
-const cartBox =
-document.getElementById("cart");
 
 
-if(cart.length === 0){
+    alert(
 
-cartBox.innerHTML =
-"No products selected.";
+        name + " added to cart!"
 
-return;
-
-}
-
-
-
-let output = "";
-
-let total = 0;
-
-
-
-cart.forEach(item=>{
-
-
-output +=
-
-`
-
-<p>
-
-${item.name}
-
-- P${item.price}
-
-</p>
-
-`;
-
-
-
-total += item.price;
-
-
-});
-
-
-
-output +=
-
-`
-
-<hr>
-
-<h3>
-
-Total: P${total}
-
-</h3>
-
-`;
-
-
-
-cartBox.innerHTML = output;
-
-
-}
-
-
-
-
-
-
-
-function createOrder(){
-
-
-if(cart.length===0){
-
-
-document.getElementById("orderResult").innerHTML=
-
-"❌ Please select a product first.";
-
-
-return;
-
-
-}
-
-
-
-let orderNumber =
-
-"LE-" +
-
-Math.floor(Math.random()*90000+10000);
-
-
-
-
-
-localStorage.setItem(
-
-"orderID",
-
-orderNumber
-
-);
-
-
-
-localStorage.setItem(
-
-"orderStatus",
-
-"Processing"
-
-);
-
-
-
-
-document.getElementById("orderResult").innerHTML=
-
-`
-
-✅ Order Created!
-
-<br><br>
-
-Order Number:
-
-<b>${orderNumber}</b>
-
-<br><br>
-
-Status:
-
-🟡 Processing
-
-<br>
-
-Estimated Delivery:
-
-5-7 Working Days
-
-<br><br>
-
-Use this order number to track your order.
-
-`;
+    );
 
 
 
