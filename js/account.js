c4c1d22b3f43c1bdaaaa96d4308937e23688212f
letsibogomarumoloa <letsibogomarumoloa@gmail.com>
2026-07-28 15:18:47 +0200

    Create account.js

---
+++ js/account.js
@@ -0,0 +1,230 @@
+/* =========================================
+   LETS ESSENTIALS
+   CUSTOMER DASHBOARD
+========================================= */
+
+
+
+const customer =
+JSON.parse(
+
+localStorage.getItem("letsCustomer")
+
+);
+
+
+
+
+
+const loggedIn =
+localStorage.getItem(
+"customerLoggedIn"
+);
+
+
+
+
+
+
+
+if(!loggedIn || !customer){
+
+
+window.location.href =
+"login.html";
+
+
+}
+
+
+
+
+
+
+
+// DISPLAY PROFILE
+
+
+document.getElementById(
+"customerName"
+).textContent =
+customer.name;
+
+
+
+document.getElementById(
+"customerEmail"
+).textContent =
+customer.email;
+
+
+
+document.getElementById(
+"customerPhone"
+).textContent =
+customer.phone;
+
+
+
+
+
+
+
+
+// DISPLAY ORDERS
+
+
+const ordersContainer =
+document.getElementById(
+"customerOrders"
+);
+
+
+
+let orders = JSON.parse(
+
+localStorage.getItem("letsOrders")
+
+) || [];
+
+
+
+
+
+
+
+const customerOrders = orders.filter(order=>{
+
+
+return order.email === customer.email;
+
+
+});
+
+
+
+
+
+
+
+
+if(customerOrders.length === 0){
+
+
+
+ordersContainer.innerHTML = `
+
+
+<p>
+You have no orders yet.
+</p>
+
+
+`;
+
+
+
+}
+
+else{
+
+
+
+customerOrders.forEach(order=>{
+
+
+ordersContainer.innerHTML += `
+
+
+
+<div class="order-result">
+
+
+<h4>
+
+Order:
+${order.id}
+
+</h4>
+
+
+
+<p>
+
+Status:
+
+<strong>
+
+${order.status}
+
+</strong>
+
+</p>
+
+
+
+<p>
+
+Items:
+
+${order.items.length}
+
+product(s)
+
+</p>
+
+
+
+</div>
+
+
+
+`;
+
+
+
+});
+
+
+
+}
+
+
+
+
+
+
+
+
+
+// LOGOUT
+
+
+const logoutBtn =
+document.getElementById(
+"logoutBtn"
+);
+
+
+
+if(logoutBtn){
+
+
+logoutBtn.addEventListener("click",()=>{
+
+
+localStorage.removeItem(
+"customerLoggedIn"
+);
+
+
+
+window.location.href =
+"login.html";
+
+
+
+});
+
+
+}
